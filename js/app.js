4858acdb7dc22447481a4fcc48cbd0240edbfab0
boni <x.bonillaa@gmail.com>
2020-11-10 21:03:45 -0500

    boni flojo

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Variables
+const resultado = document.querySelector('#resultado');
+
+// Eventos
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos();
+})
+
+
+// Funciones
+function mostrarAutos() {
+    autos.forEach(auto => {
+        const autoHTML = document.createElement('p');
+        const {marca, modelo, year, puertas, transmision, precio, color} = auto;
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmisión: ${transmision} - Precio: ${precio} - Color: ${color}
+        `;
+
+        // Insertar en el HTML
+        resultado.appendChild(autoHTML);
+    })
+}
