4500f0366a4e2415ddfaa0ab952d27d958c65c09
boni <x.bonillaa@gmail.com>
2020-11-12 21:19:47 -0500

    hay vamoo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contenedor de los resultados
+const resultado = document.querySelector('#resultado');
 
 const yearMax = new Date().getFullYear();
 const yearMin = yearMax - 10;
 
-console.log(yearMax)
-console.log(yearMin)
+// Generar un objeto con la busqueda
+const datosBusqueda = {
+    marca : '',
+    year : '',
+    minimo : '',
+    maximo : '',
+    puertas : '',
+    transmision : '',
+    color : ''
+}
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -15,6 +31,36 @@ document.addEventListener('DOMContentLoaded', () => {
     // Llena las opciones de años
     llenarSelect();
 
+})
+
+// Event listener para los select de busqueda
+marca.addEventListener('change', (e)=> {
+    datosBusqueda.marca = e.target.value;
+})
+
+year.addEventListener('change', (e)=> {
+    datosBusqueda.year = e.target.value;
+})
+
+minimo.addEventListener('change', (e)=> {
+    datosBusqueda.minimo = e.target.value;
+})
+
+maximo.addEventListener('change', (e)=> {
+    datosBusqueda.maximo = e.target.value;
+})
+
+puertas.addEventListener('change', (e)=> {
+    datosBusqueda.puertas = e.target.value;
+})
+
+transmision.addEventListener('change', (e)=> {
+    datosBusqueda.transmision = e.target.value;
+})
+
+color.addEventListener('change', (e)=> {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
 })
 
 
@@ -36,4 +82,10 @@ function mostrarAutos() {
 
 function llenarSelect() {
     
+    for ( let i = yearMax; i >= yearMin; i--) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones de año al select
+    }
 }
